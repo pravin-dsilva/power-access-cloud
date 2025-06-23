825c41a970fe4dea07822b72a0c35e03bbb6e371
Niharika Gurram <40319725+Niharika0306@users.noreply.github.com>
2025-06-23 17:09:07 +0530

    Merge pull request #76 from PDeXchange/user-guide
    Modified user guide

--- web/src/components/PopUp/NewUserGuide.jsx
+++ web/src/components/PopUp/NewUserGuide.jsx
@@ -22,13 +22,13 @@ const NewUserGuide = ({ setActionProps }) => {
       <div>
       <div className="mb-3">
           <h6>Step: 1</h6>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio architecto illo quos, quidem delectus similique fuga ratione laboriosam pariatur? Natus earum architecto, nobis impedit nemo doloremque id officiis corporis reiciendis?
+          Please add your public key in "My Keys" Section which will be used to login into the Power instance.
           <br />
           <h6>Step 2</h6>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe officiis odit quae hic ab, laudantium accusamus? Iure iusto saepe aut, accusantium sed non alias natus doloribus quod veniam ex tempore.
+          Once group access is approved, you can directly go to the "Catalog" section and choose one of the enabled catalogs as per your requirement. Click deploy.
            <br />
           <h6>Step 3: </h6>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia exercitationem et voluptatum asperiores non maxime, aperiam rem amet optio soluta, tenetur corporis vero ratione eum laborum suscipit saepe voluptatibus deserunt?
+          Enter details for your service (Power instance) and submit. Within few secs, instance will be available to access under "My Services" section.
       </div>
       </div>
     </Modal>
